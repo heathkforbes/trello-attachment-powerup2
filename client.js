cdf263c09b66478fa72c86608abad9d302f7eab8
heathkforbes <heath.forbes@nepean.com>
2026-05-15 22:28:13 +1000

    Update client.js

--- client.js
+++ client.js
@@ -1,20 +1,35 @@
-window.TrelloPowerUp.initialize({
-  "card-buttons": function (t) {
-    return [
-      {
-        text: "Download Power-Up",
-        callback: function (t) {
-          return t.card("attachments").then(function (card) {
-            return t.popup({
-              title: "Attachments",
-              url: "https://heathkforbes.github.io/trello-attachment-powerup2/attachments-v2.html?v=5",
-              args: {
-                attachments: card.attachments || []
-              }
-            });
-          });
-        }
+const APP_KEY = "9cd9af876d2fa7e06cf13dda583a6575";
+const APP_NAME = "Attachment JSON Test";
+const APP_AUTHOR = "Heath Forbes";
+
+function openDownloader(t) {
+  return t.card("id", "name", "attachments").then(function (card) {
+    return t.popup({
+      title: "Attachments",
+      url: "https://heathkforbes.github.io/trello-attachment-powerup2/attachments-v2.html?v=6",
+      args: {
+        cardId: card.id,
+        cardName: card.name,
+        attachments: card.attachments || []
       }
-    ];
+    });
+  });
+}
+
+window.TrelloPowerUp.initialize(
+  {
+    "card-buttons": function (t) {
+      return [
+        {
+          text: "Download Power-Up",
+          callback: openDownloader
+        }
+      ];
+    }
+  },
+  {
+    appKey: APP_KEY,
+    appName: APP_NAME,
+    appAuthor: APP_AUTHOR
   }
-});
+);
