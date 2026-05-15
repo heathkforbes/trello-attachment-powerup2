5365f0503bf006260977548d334faae3016a3b73
heathkforbes <heath.forbes@nepean.com>
2026-05-15 17:49:26 +1000

    Update client.js

--- client.js
+++ client.js
@@ -6,8 +6,8 @@ window.TrelloPowerUp.initialize({
         callback: function (t) {
           return t.card("attachments").then(function (card) {
             return t.popup({
-              title: "Attachments JSON",
-              url: "https://heathkforbes.github.io/trello-attachment-powerup2/attachments-v2.html?v=4",
+              title: "Attachments",
+              url: "https://heathkforbes.github.io/trello-attachment-powerup2/attachments-v2.html?v=5",
               args: {
                 attachments: card.attachments || []
               }
